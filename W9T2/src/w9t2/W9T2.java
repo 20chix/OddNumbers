1b6f9d64a3e191ffb671eec8aed80784be75a3ae
Hadi <hadi20107@hotmail.co.uk>
2016-09-27 09:41:50 +0100

    Add files via upload

---
+++ W9T2/src/w9t2/W9T2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w9t2;
+
+/**
+ *
+ * @author hadielmekawi
+ */
+public class W9T2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int limit =50;
+        int i;
+        
+        for(i=1;i<=limit;i++){
+        
+        if(  i % 2 != 0){
+                                System.out.println(i+ "  are odd numbers between 0 and 50 ");
+                        }
+        
+        }
+    }
+    
+}
